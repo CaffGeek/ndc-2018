acd67640f6578b2babb1bad2234f919f6bf45e3b
Chad Hurd <gibble@gmail.com>
2018-05-07 13:03:58 -0500

    with knn and abs-distance functions

--- day-1/Part1.attempt.fsx
+++ day-1/Part1.attempt.fsx
@@ -2,57 +2,73 @@
 open System
 open System.IO
 open System.Linq
-let localPath = __SOURCE_DIRECTORY__
-let dataPath = Path.Combine(localPath,"digits")
-let fileName = Path.Combine(dataPath, "trainingsample.csv")
-let data = File.ReadAllLines(fileName)
+open System.Collections.Generic
 
-// 2. EXTRACTING COLUMNS
-let lines = data |> Array.map(fun s -> s.Split(','))
-
-// 3. CLEANING UP HEADERS
-let headers = lines.[0]
-let dataLines = lines.[1..]
-
-// 4. CONVERTING FROM STRINGS TO INTS
-let ints = dataLines |> Array.map(fun x -> x |> Array.map(fun s -> (int)s))
-
-// 5. CONVERTING ARRAYS TO RECORDS
 type LabelPixel = { Label:int; Pixels:int[] }
-let records = ints |> Array.map(fun x -> { Label = x.[0]; Pixels = x.[1..]})
+let fileAsRecords (fileName: string) =
+    let localPath = __SOURCE_DIRECTORY__
+    let dataPath = Path.Combine(localPath,"digits")
+    let fileName = Path.Combine(dataPath, fileName)
+    let data = File.ReadAllLines(fileName)
+    // 2. EXTRACTING COLUMNS
+    let lines = data |> Array.map(fun s -> s.Split(','))
+    // 3. CLEANING UP HEADERS
+    let dataLines = lines.[1..]
+    // 4. CONVERTING FROM STRINGS TO INTS
+    let ints = dataLines |> Array.map(fun x -> x |> Array.map(fun s -> (int)s))
+    // 5. CONVERTING ARRAYS TO RECORDS
+    ints |> Array.map(fun x -> { Label = x.[0]; Pixels = x.[1..]})
 
 // 6. COMPUTING DISTANCES
-let distance (P1: int[]) (P2: int[]) =
+let distance1 (P1: int[]) (P2: int[]) =
     Array.map2 (fun p1 p2 -> (p1-p2)*(p1-p2)) P1 P2
     |> Array.sum
     |> float
     |> Math.Sqrt
 
+let distance2 (P1: int[]) (P2: int[]) =
+    Array.map2 (fun p1 p2 -> Math.Abs((int)p1-p2)) P1 P2
+    |> Array.sum
+    |> float
+
 // 7. WRITING THE CLASSIFIER FUNCTION
 type Distance = { Label: int; Distance: float }
-let classify (unknown:int[]) =
-    let prediction = 
-        records 
-        |> Array.map(fun r -> { Label = r.Label; Distance = distance r.Pixels unknown })
-        |> Array.minBy(fun x -> x.Distance)
-    prediction.Label
+
+let mode (l:int[]) =
+    let x = 
+        l
+        |> Array.countBy (fun item -> item)             // Count individual items
+        |> Array.fold                                   // Find max counts
+            (fun (cp, lst) (item, c) ->                 // State is (count, list of items with that count)
+                if c > cp then (c, [item])              // New max - keep count and a list of the single item
+                elif c = cp then (c, item :: lst)       // New element with max count - prepend it to the list
+                else (cp,lst))                          // else just keep old count/list
+            (0, [])                                     // Start with a count of 0 and a dummy item
+        |> snd                                          // From (count, list) we just want the second item (the list)
+    x.[0]
+
+let knnclassify (k:int) (trainedRecords: LabelPixel[]) (unknown:int[]) =
+    let sorted = 
+        trainedRecords 
+        |> Array.map(fun r -> { Label = r.Label; Distance = distance2 r.Pixels unknown })
+        |> Array.sortBy(fun x-> x.Distance)
+    let top = 
+        sorted.[0..k-1]
+        |> Array.map(fun x -> x.Label)
+    mode top
 
 // 8. EVALUATING THE MODEL AGAINST VALIDATION DATA
+let classifier = 
+    fileAsRecords "trainingsample.csv"
+    |> knnclassify 3
 
-let validationFileName = Path.Combine(dataPath, "validationsample.csv")
-let validationData = File.ReadAllLines(validationFileName)
+type Result = { Actual: int; Predicted: int }
 
-type Result = { Actual: int; Predicted: int}
 let validationRecords = 
-    validationData.[1..] 
-    |> Array.map(fun s -> s.Split(','))
-    |> Array.map(fun x -> x |> Array.map(fun s -> (int)s))
-    |> Array.map(fun x -> { Label = x.[0]; Pixels = x.[1..] })
-    |> Array.map(fun x -> { Actual = x.Label; Predicted = classify x.Pixels })
-
+    fileAsRecords "validationsample.csv"
+    |> Array.map(fun x -> { Actual = x.Label; Predicted = classifier x.Pixels })
 let passes = 
     validationRecords
     |> Array.filter(fun x -> x.Actual = x.Predicted)
-    
 let percentage =
     (float)passes.Length / (float)validationRecords.Length
